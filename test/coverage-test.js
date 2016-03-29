4d5e7c11ad465451b33251ba3ebe0144ab85900e
Adam <amarinelli@esri.ca>
2016-03-29 12:25:15 -0400

    Test to check for coverage schema duplicates

---
+++ test/coverage-test.js
@@ -0,0 +1,29 @@
+// Search every schema name assigned in ../coverage-config.js and check for duplicates
+
+var buildCoverage = require('../coverage-config.js');
+
+schemaListFull = [];
+schemaListDuplicates = [];
+
+function searchChildElements(element, index, array) {
+  if ('children' in element) {
+    element.children.forEach(searchChildElements)
+  } else {
+    if (schemaListFull.indexOf(element.schema) != -1) {
+      console.log("Duplicate found: " + element.schema);
+      schemaListDuplicates.push(element.schema);
+    } else {
+      schemaListFull.push(element.schema);
+    }
+  }
+}
+
+buildCoverage.forEach(function(element, index, array) {
+  searchChildElements(element, index, array)
+});
+
+if (schemaListDuplicates.length > 0) {
+  console.warn(`Coverage Tests completed, ${schemaListDuplicates.length} DUPLICATES FOUND`);
+} else {
+  console.log('\nCoverage Tests completed, no duplicates found');
+}
