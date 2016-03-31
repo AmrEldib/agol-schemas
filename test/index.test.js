692e7ce92c88a009f9e8a8515db40ec909f73e3c
Amr Eldib <eldib.amr@gmail.com>
2016-03-30 22:08:55 -0700

    Added test to validate output schemas
    Closes #11. Modified test for coverage-config for better error printing. Added test for listAllSchemas

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,8 @@
-﻿
+﻿var assert = require('assert');
+var agolSchemas = require('../index');
+
+describe("agol-schemas", function () {
+  it("Lists all schemas", function () {
+    assert.notEqual(agolSchemas.listAllSchemas().length, 0);
+  });
+});
